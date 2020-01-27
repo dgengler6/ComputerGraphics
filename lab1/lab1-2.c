d7bd9af7ef123fedb7e99f42236615997b36d986
OhLeMatt <matthieu.verdet@epfl.ch>
2020-01-27 13:03:14 +0100

    trsl useless

--- lab1/lab1-2.c
+++ lab1/lab1-2.c
@@ -10,7 +10,7 @@
 #ifdef __APPLE__
 	#define GL_SILENCE_DEPRECATION
 	#include <OpenGL/gl3.h>
-	
+
 	// Linking hint for Lightweight IDE
 	// uses framework Cocoa
 #endif
@@ -65,9 +65,9 @@ void init(void)
 
 	program = loadShaders("lab1-2.vert","lab1-2.frag");
 	printError("init shader");
-	
+
 	// Upload geometry to the GPU:
-	
+
 	// Allocate and activate Vertex Array Object
 	glGenVertexArrays(1, &vertexArrayObjID);
 	glBindVertexArray(vertexArrayObjID);
@@ -77,20 +77,20 @@ void init(void)
 
 	// Allocate Vertex Buffer Objects
 	glGenBuffers(1, &vertexBufferObjIDColor);
-	
+
 	// VBO for vertex data
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObjID);
 	glBufferData(GL_ARRAY_BUFFER, 12*sizeof(GLfloat), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(glGetAttribLocation(program, "in_Position"), 3, GL_FLOAT, GL_FALSE, 0, 0); 
+	glVertexAttribPointer(glGetAttribLocation(program, "in_Position"), 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(glGetAttribLocation(program, "in_Position"));
 
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObjIDColor);
 	glBufferData(GL_ARRAY_BUFFER, 9*sizeof(GLfloat), colors, GL_STATIC_DRAW);
-	glVertexAttribPointer(glGetAttribLocation(program, "in_Color"), 3, GL_FLOAT, GL_FALSE, 0, 0); 
+	glVertexAttribPointer(glGetAttribLocation(program, "in_Color"), 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(glGetAttribLocation(program, "in_Color"));
 
     glUniformMatrix4fv(glGetUniformLocation(program, "myMatrix"), 1, GL_TRUE, myMatrix);
-	
+
 	// End of upload of geometry
 	printError("init arrays");
 }
@@ -105,8 +105,8 @@ void display(void)
 	glBindVertexArray(vertexArrayObjID);	// Select VAO
 	glDrawArrays(GL_TRIANGLES, 0,3);	// draw object
 	printError("display");
-	
-	
+
+
 	glutSwapBuffers();
 }
 
@@ -115,7 +115,7 @@ int main(int argc, char *argv[])
 	glutInit(&argc, argv);
 	glutInitContextVersion(3, 2);
 	glutCreateWindow ("GL3 white triangle example");
-	glutDisplayFunc(display); 
+	glutDisplayFunc(display);
 	init ();
 	glutMainLoop();
 	return 0;
