0f5f8877c78a65afc8340c8000834f07af7bdbe4
OhLeMatt <matthieu.verdet@epfl.ch>
2020-02-10 14:22:48 +0100

    Setup pour import

--- lab3/lab3-1.c
+++ lab3/lab3-1.c
@@ -1,12 +1,3 @@
-// Lab 1-1.
-// This is the same as the first simple example in the course book,
-// but with a few error checks.
-// Remember to copy your file to a new on appropriate places during the lab so you keep old results.
-// Note that the files "lab1-1.frag", "lab1-1.vert" are required.
-
-// Should work as is on Linux and Mac. MS Windows needs GLEW or glee.
-// See separate Visual Studio version of my demos.
-
 #ifdef __APPLE__
 	#define GL_SILENCE_DEPRECATION
 	#include <OpenGL/gl3.h>
@@ -29,7 +20,7 @@
 GLuint program;
 
 // Model and Textures
-Model *m;
+Model * wb, * wr, * ww, * b;
 GLuint texture;
 
 #define near 1.0
@@ -39,40 +30,63 @@ GLuint texture;
 #define top 0.5
 #define bottom -0.5
 
+#define buffer_object(NAME) \
+	unsigned int NAME ## VertexBufferObjID; \
+	unsigned int NAME ## IndexBufferObjID; \
+	unsigned int NAME ## NormalBufferObjID; \
+	unsigned int NAME ## TexCoordBufferObjID; \
+	glGenBuffers(1, &(NAME ## VertexBufferObjID)); \
+	glGenBuffers(1, &(NAME ## IndexBufferObjID)); \
+	glGenBuffers(1, &(NAME ## NormalBufferObjID)); \
+	glGenBuffers(1, &(NAME ## TexCoordBufferObjID));
+
+#define buffer_full_setup(MODEL, NAME, MODEL_STRING) \
+	buffer_object(NAME); \
+	MODEL=LoadModel(MODEL_STRING); \
+	glGenVertexArrays(1, &(NAME ## VertexArrayObjID)); \
+	glBindVertexArray(NAME ## VertexArrayObjID); \
+	if (MODEL->texCoordArray != NULL) \
+		buffer_setup("in_TexCoord", NAME ## TexCoordBufferObjID, MODEL->texCoordArray, MODEL->numVertices, 2); \
+	buffer_setup("in_Position", NAME ## VertexBufferObjID, MODEL->vertexArray, MODEL->numVertices, 3); \
+	buffer_setup("in_Normal", NAME ## NormalBufferObjID, MODEL->normalArray, MODEL->numVertices, 3); \
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, NAME ## IndexBufferObjID); \
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, MODEL->numIndices*sizeof(GLuint), MODEL->indexArray, GL_STATIC_DRAW);
+
+void buffer_setup(char * in_shader, unsigned int buffer_object, const void * array, GLsizeiptr size , int dim);
+
+void buffer_setup(char * in_shader, unsigned int buffer_object, const void * array, GLsizeiptr size , int dim){
+	glBindBuffer(GL_ARRAY_BUFFER, buffer_object);
+	glBufferData(GL_ARRAY_BUFFER, size*dim*sizeof(GLfloat), array, GL_STATIC_DRAW);
+	glVertexAttribPointer(glGetAttribLocation(program, in_shader), dim, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(glGetAttribLocation(program, in_shader));
+}
+
 mat4 projectionMatrix;
 
 mat4 camMatrix;
 
 // vertex array object
-    unsigned int bunnyVertexArrayObjID;
+    unsigned int WBVertexArrayObjID, WRVertexArrayObjID, WWVertexArrayObjID, BVertexArrayObjID;
 
 void init(void)
 {
 	// vertex buffer object, used for uploading the geometry
 
-    unsigned int bunnyVertexBufferObjID;
-    unsigned int bunnyIndexBufferObjID;
-    unsigned int bunnyNormalBufferObjID;
-    unsigned int projectionMatrixBufferObjID;
-    unsigned int bunnyTexCoordBufferObjID;
-
-
+  unsigned int projectionMatrixBufferObjID;
 	dumpInfo();
 
 	// GL inits
 	glClearColor(1,0.2,1,0);
-
-
 	printError("GL inits");
-
 	// Load and compile shader
-
 	program = loadShaders("lab3-1.vert","lab3-1.frag");
 	printError("init shader");
 
 	// Upload geometry to the GPU:
-    m=LoadModel("bunnyplus.obj");
-
+	buffer_full_setup(wb, WB, "windmill/windmill-balcony.obj");
+	buffer_full_setup(wr, WR, "windmill/windmill-roof.obj");
+	buffer_full_setup(ww, WW, "windmill/windmill-walls.obj");
+	buffer_full_setup(b, B, "windmill/blade.obj");
 
 		projectionMatrix = frustum(left, right, bottom, top, near, far);
 
@@ -80,6 +94,7 @@ void init(void)
 		vec3 l = SetVector(0,0,-3);
 		vec3 v = SetVector(0,1,0);
 		camMatrix = lookAtv(p, l, v);
+
     // Load and bind the texture
     LoadTGATextureSimple("quack.tga", &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
@@ -87,37 +102,10 @@ void init(void)
     glActiveTexture(GL_TEXTURE0);
 
 	// Allocate and activate Vertex Array Object
-    glGenVertexArrays(1, &bunnyVertexArrayObjID);
-    glGenBuffers(1, &bunnyVertexBufferObjID);
-    glGenBuffers(1, &bunnyIndexBufferObjID);
-    glGenBuffers(1, &bunnyNormalBufferObjID);
-    glGenBuffers(1, &bunnyTexCoordBufferObjID);
+
+
     glGenBuffers(1, &projectionMatrixBufferObjID);
 
-    glBindVertexArray(bunnyVertexArrayObjID);
-
-    if (m->texCoordArray != NULL)
-    {
-        glBindBuffer(GL_ARRAY_BUFFER, bunnyTexCoordBufferObjID);
-        glBufferData(GL_ARRAY_BUFFER, m->numVertices*2*sizeof(GLfloat), m->texCoordArray, GL_STATIC_DRAW);
-        glVertexAttribPointer(glGetAttribLocation(program, "in_TexCoord"), 2, GL_FLOAT, GL_FALSE, 0, 0);
-        glEnableVertexAttribArray(glGetAttribLocation(program, "in_TexCoord"));
-    }
-
-    // VBO for vertex data
-    glBindBuffer(GL_ARRAY_BUFFER, bunnyVertexBufferObjID);
-    glBufferData(GL_ARRAY_BUFFER, m->numVertices*3*sizeof(GLfloat), m->vertexArray, GL_STATIC_DRAW);
-    glVertexAttribPointer(glGetAttribLocation(program, "in_Position"), 3, GL_FLOAT, GL_FALSE, 0, 0);
-    glEnableVertexAttribArray(glGetAttribLocation(program, "in_Position"));
-
-    // VBO for normal data
-    glBindBuffer(GL_ARRAY_BUFFER, bunnyNormalBufferObjID);
-    glBufferData(GL_ARRAY_BUFFER, m->numVertices*3*sizeof(GLfloat), m->normalArray, GL_STATIC_DRAW);
-    glVertexAttribPointer(glGetAttribLocation(program, "in_Normal"), 3, GL_FLOAT, GL_FALSE, 0, 0);
-    glEnableVertexAttribArray(glGetAttribLocation(program, "in_Normal"));
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, bunnyIndexBufferObjID);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, m->numIndices*sizeof(GLuint), m->indexArray, GL_STATIC_DRAW);
 
 
 
@@ -149,9 +137,12 @@ void display(void)
 		glUniformMatrix4fv(glGetUniformLocation(program, "vingthuit"), 1, GL_TRUE, packed.m);
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glBindVertexArray(bunnyVertexArrayObjID);    // Select VAO
+	glBindVertexArray(WBVertexArrayObjID);    // Select VAO
+	glBindVertexArray(WRVertexArrayObjID);
+	glBindVertexArray(WWVertexArrayObjID);
+	glBindVertexArray(BVertexArrayObjID);
 
-    glDrawElements(GL_TRIANGLES, m->numIndices, GL_UNSIGNED_INT, 0L);
+    glDrawElements(GL_TRIANGLES, wb->numIndices, GL_UNSIGNED_INT, 0L);
 	printError("display");
 
 
